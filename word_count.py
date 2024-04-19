623a0443871fa50a9a5c9a208137ae688992854b
advaittakalkar <30879573+advaittakalkar@users.noreply.github.com>
2024-04-19 11:28:42 +0530

    Add files via upload
    word count program

---
+++ word_count.py
@@ -0,0 +1,21 @@
+from pyspark import SparkContext
+
+
+# common lines
+sc = SparkContext("local[*]", "word_count" )
+i = sc.textFile("C:/World/Learnings/Big data/5. spark/Week 9/file.txt.txt")
+
+# one input row multiple output
+rdd = i.flatMap(lambda x: x.split(" "))
+
+# one input one output
+rdd2 = rdd.map(lambda x: (x, 1))
+
+# reduce the number of rows - take two rows and does aggregation on keys
+rdd3 = rdd2.reduceByKey(lambda x, y: x + y)
+
+# Action
+rdd4 = rdd3.collect()
+
+for a in rdd4:
+    print(a)
