971dd2d5ab289cbb8300289a35fa1fe457b71b47
advaittakalkar <advait.takalkar@gmail.com>
2024-06-08 18:55:32 +0530

    completed this program

--- 6. avg_friends_for_each_age_using_function.py
+++ 6. avg_friends_for_each_age_using_function.py
@@ -14,10 +14,16 @@ sc = SparkContext("local[*]", "avg_friends_for_age")
 
 rdd1 = sc.textFile("C:/World/Learnings/Big data/5. spark/Week 9/friendsdata.csv")
 
-rdd2 = rdd1.map(parse_line)
+rdd2 = rdd1.map(parse_line).mapValues(lambda x: (float(x),1))
 # output - (33,345) we want (33,(345,1))
 
-for a in rdd2.collect():
+rdd3 = rdd2.reduceByKey(lambda x, y: ((x[0]+y[0],x[1]+y[1])))   # ignoring key here
+
+rdd4 = rdd3.map(lambda x: (x[0],(x[1][0]/x[1][1])))  #x[0] is key here
+#rdd4 = rdd3.mapValues(lambda x : (x[0]/x[1]))
+
+
+for a in rdd4.collect():
     print(a)
 
 
